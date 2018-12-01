7c84edb13c6365e80deba3d3cae7d648982e8144
BushuoPaul Buschmann <paul.buschmann@chello.at>
2018-12-01 18:06:54 +0100

    first commit skelleton

---
+++ src/components/task.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+
+const Task = () => {
+  return (
+    <div className="m-2">
+      <Button color="secondary">GOGOGO</Button>
+    </div>
+  );
+};
+
+export default Task;
